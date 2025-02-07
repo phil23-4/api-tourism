ee2b75ed449c636e3b9a3e90d90fae85a2ac35ce
Phillip Francis Chapita <philoc95@live.com>
2025-02-07 12:16:05 +0200

    Function to filter the body object when updating user data

---
+++ src/utils/filter.js
@@ -0,0 +1,10 @@
+// Function to filter the body object when updating user data to restrict some fields from being manipulated by the user
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
+module.exports = filterObj;
