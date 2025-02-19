88f24e0074e0c85a6c0632245b2f92e635c3efd2
Phillip Francis Chapita <philoc95@live.com>
2025-02-19 23:13:27 +0200

    minor change to filename

--- src/utils/sharp.js
+++ src/utils/sharp.js
@@ -25,7 +25,7 @@ exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
 exports.resizeCoverPhoto = catchAsync(async (req, res, next) => {
   if (!req.file) return next();
 
-  req.file.filename = `destination-${req.params.destinationId}-${Date.now()}.jpeg`;
+  req.file.filename = `destination-${req.params.destinationId}-${Date.now()}-cover.jpeg`;
 
   await sharp(req.file.buffer)
     .resize({
@@ -58,7 +58,7 @@ exports.resizeTourImages = catchAsync(async (req, res, next) => {
 
   await Promise.all(
     req.files.images.map(async (file, i) => {
-      const filename = `tour-${req.params.attractionId}-${Date.now()}-${i + 1}.webp`;
+      const filename = `tour-${req.params.tourId}-${Date.now()}-${i + 1}.webp`;
       logger.info(filename);
 
       await sharp(file.buffer)
@@ -66,6 +66,58 @@ exports.resizeTourImages = catchAsync(async (req, res, next) => {
         .toFormat('webp')
         .webp({ quality: 80 })
         .toFile(`src/public/assets/img/tours/${filename}`);
+
+      req.body.images.push(filename);
+    })
+  );
+
+  next();
+});
+
+exports.resizeAttractionCover = catchAsync(async (req, res, next) => {
+  if (!req.file) return next();
+
+  req.file.filename = `attraction-${req.params.attractionId}-${Date.now()}-cover.jpeg`;
+
+  await sharp(req.file.buffer)
+    .resize({
+      width: 2000,
+      height: 1333,
+      kernel: sharp.kernel.nearest,
+      fit: sharp.fit.cover,
+      position: sharp.strategy.entropy,
+    })
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`src/public/assets/img/attractions/${req.file.filename}`);
+
+  next();
+});
+
+exports.resizeAttractionImages = catchAsync(async (req, res, next) => {
+  if (!req.files.mainImage || !req.files.images) return next();
+
+  // 1) Cover image
+  req.body.mainImage = `tour-${req.params.attractionId}-${Date.now()}-cover.jpeg`;
+  await sharp(req.files.mainImage[0].buffer)
+    .resize(2000, 1333)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`src/public/assets/img/attractions/${req.body.mainImage}`);
+
+  // 2) Images
+  req.body.images = [];
+
+  await Promise.all(
+    req.files.images.map(async (file, i) => {
+      const filename = `attraction-${req.params.attractionId}-${Date.now()}-${i + 1}.webp`;
+      logger.info(filename);
+
+      await sharp(file.buffer)
+        .resize(2000, 1333)
+        .toFormat('webp')
+        .webp({ quality: 80 })
+        .toFile(`src/public/assets/img/attractions/${filename}`);
 
       req.body.images.push(filename);
     })
