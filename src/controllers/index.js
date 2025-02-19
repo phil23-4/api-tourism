75f565fd25b2422f5327c98ce0d7d66b1b7662e6
Phillip Francis Chapita <philoc95@live.com>
2025-02-19 23:25:50 +0200

    added attraction controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,3 +2,4 @@ module.exports.authController = require('./auth.controller');
 module.exports.userController = require('./user.controller');
 module.exports.profileController = require('./profile.controller');
 module.exports.destinationController = require('./destination.controller');
+module.exports.attractionController = require('./attraction.controller');
