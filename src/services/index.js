99384f4acd67e3cc9c687b398304e7378dd633e0
Phillip Francis Chapita <philoc95@live.com>
2025-02-19 23:24:57 +0200

    added attraction service to the index file

--- src/services/index.js
+++ src/services/index.js
@@ -4,4 +4,5 @@ module.exports.tokenService = require('./token.service');
 module.exports.userService = require('./user.service');
 module.exports.profileService = require('./profile.service');
 module.exports.distanceService = require('./distance.service');
+module.exports.attractionService = require('./attraction.service');
 module.exports.factoryService = require('./factory.service');
