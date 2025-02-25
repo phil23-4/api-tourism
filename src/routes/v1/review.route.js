d36824e2edc949816ce6ce1fd523768aeb454bf9
Phillip Francis Chapita <philoc95@live.com>
2025-02-25 19:26:43 +0200

    Add GET endpoint for retrieving reviews and update review route

--- src/routes/v1/review.route.js
+++ src/routes/v1/review.route.js
@@ -6,6 +6,9 @@ const { reviewValidation } = require('../../validations');
 
 const router = express.Router({ mergeParams: true });
 
-router.route('/').post(auth('manageUser'), validate(reviewValidation.createReview), reviewController.createReview);
+router
+  .route('/')
+  .post(auth('manageUser'), validate(reviewValidation.createReview), reviewController.createReview)
+  .get(auth('getUsers'), validate(reviewValidation.getReviews), reviewController.getReviews);
 
 module.exports = router;
