2441dc0cc959c22d4d10f2aad2ae9c90a8deb5c6
Phillip Francis Chapita <philoc95@live.com>
2025-02-26 12:39:59 +0200

    Add getReviews function to retrieve user reviews with filtering and pagination

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,15 +2,16 @@ const httpStatus = require('http-status');
 const pick = require('../utils/pick');
 const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
-const { User, Profile } = require('../models');
+const { User, Profile, Review } = require('../models');
 const { userService, factoryService } = require('../services');
 
 /**
- * @desc      Create New User
- * @param     { Object } req - Request object
- * @param     { Object } res - Response object
- * @property  { Object } req.body - Body object data
- * @returns   { JSON } - A JSON object representing the status and user
+ * Creates a new user.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} req.body - The body of the request containing user data.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A promise that resolves when the user is created and the response is sent.
  */
 const createUser = catchAsync(async (req, res) => {
   const user = await userService.createUser(req.body);
@@ -18,15 +19,17 @@ const createUser = catchAsync(async (req, res) => {
 });
 
 /**
- * @desc      Get All Users
- * @param     { Object } req - Request object
- * @param     { Object } res - Response object
- * @property  { Object } filter - Mongo filter to Select specific fields
- * @property  { Object } options - Query options
- * @property  { String } [options.sortBy] - Sort option in the format: sortField:(desc|asc) to Sort returned data
- * @property  { Number } [options.limit] - Maximum number of results per page (default = 10)
- * @property  { Number } [options.page] - Current page (default = 1)
- * @returns   { JSON } - A JSON object representing the status and users
+ * Get users based on query parameters.
+ *
+ * @param {Object} req - Express request object
+ * @param {Object} req.query - Query parameters
+ * @param {string} [req.query.username] - Username to filter users
+ * @param {string} [req.query.role] - Role to filter users
+ * @param {string} [req.query.sortBy] - Sorting criteria
+ * @param {number} [req.query.limit] - Maximum number of users to return
+ * @param {number} [req.query.page] - Page number for pagination
+ * @param {Object} res - Express response object
+ * @returns {Promise<void>} - Returns a promise that resolves to void
  */
 const getUsers = catchAsync(async (req, res) => {
   const filter = pick(req.query, ['username', 'role']);
@@ -36,11 +39,13 @@ const getUsers = catchAsync(async (req, res) => {
 });
 
 /**
- * @desc      Get User Data Using It's ID Controller
- * @param     { Object } req - Request object
- * @param     { Object } res - Response object
- * @property  { String } req.params.userId - User ID
- * @returns   { JSON } - A JSON object representing the status, and User data
+ * Get user by ID
+ * @param {Object} req - Express request object
+ * @param {Object} req.user - User object attached to the request
+ * @param {string} req.user.id - ID of the user to retrieve
+ * @param {Object} res - Express response object
+ * @returns {Promise<void>}
+ * @throws {ApiError} - If user is not found
  */
 const getUser = catchAsync(async (req, res) => {
   const user = await factoryService.getDocById(User, req.user.id, { path: 'profile' });
@@ -51,12 +56,50 @@ const getUser = catchAsync(async (req, res) => {
 });
 
 /**
- * @desc      Update User Details
- * @param     { Object } req - Request object
- * @param     { Object } res - Response object
- * @property  { String } req.params.userId - User ID
- * @property  { Object } req.body - Body object data
- * @returns   { JSON } - A JSON object representing the status and the user
+ * Get reviews based on query parameters.
+ * If no user is specified in the query, the logged-in user's ID is used.
+ * Filters reviews based on rating, user, attraction, and tour.
+ * Supports sorting, pagination, and limiting the number of results.
+ * Throws an error if no reviews are found.
+ *
+ * @param {Object} req - Express request object
+ * @param {Object} req.query - Query parameters
+ * @param {string} [req.query.user] - User ID to filter reviews
+ * @param {string} [req.query.rating] - Rating to filter reviews
+ * @param {string} [req.query.attraction] - Attraction ID to filter reviews
+ * @param {string} [req.query.tour] - Tour ID to filter reviews
+ * @param {string} [req.query.sortBy] - Sorting criteria
+ * @param {number} [req.query.limit] - Limit the number of results
+ * @param {number} [req.query.page] - Page number for pagination
+ * @param {Object} res - Express response object
+ * @returns {Promise<void>} - Returns a promise that resolves to void
+ * @throws {ApiError} - Throws an error if no reviews are found
+ */
+const getReviews = catchAsync(async (req, res) => {
+  if (!req.query.user) req.query.user = req.user.id;
+  const filter = pick(req.query, ['rating', 'user', 'attraction', 'tour']);
+  const options = pick(req.query, ['sortBy', 'limit', 'page']);
+  const review = await factoryService.queryAll(Review, filter, options);
+  if (review.results.length === 0) throw new ApiError(httpStatus.NOT_FOUND, 'No Reviews Found');
+  res.status(httpStatus.OK).json({ message: 'success', review });
+});
+
+/**
+ * Update user details.
+ *
+ * This function handles the updating of user details except for the password.
+ * If an attempt is made to update the password, an error is thrown.
+ *
+ * @param {Object} req - Express request object
+ * @param {Object} req.body - The request body containing user details to update
+ * @param {string} req.body.password - The password field which should not be updated
+ * @param {Object} req.params - The request parameters
+ * @param {string} req.params.userId - The ID of the user to update
+ * @param {Object} res - Express response object
+ *
+ * @throws {ApiError} If the request body contains a password field
+ *
+ * @returns {Promise<void>} Sends the updated user details in the response
  */
 const updateUser = catchAsync(async (req, res) => {
   // 1) Create an error if user tries to update password data
@@ -68,11 +111,14 @@ const updateUser = catchAsync(async (req, res) => {
 });
 
 /**
- * @desc      Delete User Using It's ID Controller
- * @param     { Object } req - Request object
- * @param     { Object } res - Response object
- * @property  { String } req.params.userId - User ID
- * @returns   { JSON } - An empty JSON object
+ * Delete a user and their associated profile.
+ *
+ * @param {Object} req - Express request object.
+ * @param {Object} req.params - Request parameters.
+ * @param {string} req.params.userId - ID of the user to delete.
+ * @param {Object} req.user - Authenticated user object.
+ * @param {Object} res - Express response object.
+ * @returns {Promise<void>} - A promise that resolves when the user and profile are deleted.
  */
 const deleteUser = catchAsync(async (req, res) => {
   const user = await factoryService.getDocById(User, req.user.id);
@@ -85,11 +131,16 @@ const deleteUser = catchAsync(async (req, res) => {
 });
 
 /**
- * @desc      Delete LoggedIn User's Data Controller
- * @param     { Object } req - Request object
- * @param     { Object } res - Response object
- * @property  { Object } req.user.id - An object contains logged in user data
- * @returns   { JSON } - A JSON object representing the status and message
+ * Deactivates the user's account and updates the profile status.
+ *
+ * @function deleteMyAccount
+ * @async
+ * @param {Object} req - Express request object.
+ * @param {Object} req.user - User object attached to the request.
+ * @param {string} req.user.id - ID of the user.
+ * @param {Object} res - Express response object.
+ * @throws {ApiError} If the account does not exist.
+ * @returns {void}
  */
 const deleteMyAccount = catchAsync(async (req, res) => {
   // Find user document and deactivate it
@@ -105,6 +156,7 @@ module.exports = {
   createUser,
   getUsers,
   getUser,
+  getReviews,
   updateUser,
   deleteUser,
   deleteMyAccount,
