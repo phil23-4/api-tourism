eaccd4448436b68aab5bf85244a759d0d0275e59
Phillip Francis Chapita <philoc95@live.com>
2025-02-26 23:44:00 +0200

    Enhance tour validation schema: enforce required fields and improve structure for better data integrity

--- src/validations/tour.validation.js
+++ src/validations/tour.validation.js
@@ -3,7 +3,7 @@ const { objectId } = require('./custom.validation');
 
 const createTour = {
   params: Joi.object().keys({
-    attractionId: Joi.string().custom(objectId),
+    attractionId: Joi.string().custom(objectId).required(),
   }),
   body: Joi.object().keys({
     name: Joi.string().required(),
@@ -11,6 +11,89 @@ const createTour = {
     maxGroupSize: Joi.number().required(),
     difficulty: Joi.string().required().valid('easy', 'medium', 'difficult'),
     price: Joi.number().required(),
+    priceDiscount: Joi.number(),
+    summary: Joi.string().required(),
+    description: Joi.string(),
+    mainImage: Joi.object().keys({
+      url: Joi.string(),
+      publicId: Joi.string(),
+    }),
+    images: Joi.array().items(
+      Joi.object().keys({
+        url: Joi.string(),
+        publicId: Joi.string(),
+      })
+    ),
+    startDates: Joi.array().items(Joi.date().format('DD/MM/YYYY')).required(),
+    secretTour: Joi.boolean().optional(),
+    location: Joi.object().keys({
+      type: Joi.string().required(),
+      coordinates: Joi.array().items(Joi.number().precision(8)),
+      name: Joi.string(),
+      description: Joi.string(),
+      address: Joi.string(),
+      day: Joi.number().required(),
+    }),
+    locations: Joi.array()
+      .items({
+        description: Joi.string().required(),
+        coordinates: Joi.array().items(Joi.number()).required(),
+        day: Joi.number().required(),
+      })
+      .required(),
+    guides: Joi.array().items(Joi.string().custom(objectId)).required(),
+    attraction: Joi.string().custom(objectId).required(),
+  }),
+};
+
+const getTours = {
+  query: Joi.object().keys({
+    name: Joi.string(),
+    ratingsAverage: Joi.string(),
+    rating: Joi.alternatives().try(
+      Joi.number().min(1).max(5),
+      Joi.object({
+        gte: Joi.number().min(1),
+        lte: Joi.number().max(5),
+      })
+    ),
+    slug: Joi.string(),
+    attraction: Joi.string().custom(objectId),
+    sortBy: Joi.string().valid(
+      'rating',
+      'ratingsAverage',
+      'attraction',
+      'difficulty',
+      'duration',
+      'maxGroupSize',
+      'price',
+      'name'
+    ),
+    limit: Joi.number().integer().min(1).max(100),
+    page: Joi.number().integer().min(1),
+  }),
+};
+
+const getTour = {
+  params: Joi.object()
+    .keys({
+      tourId: Joi.string().custom(objectId).required(),
+    })
+    .messages({
+      'object.missing': 'Must specify a tourId',
+    }),
+};
+
+const updateTour = {
+  params: Joi.object().keys({
+    tourId: Joi.required().custom(objectId),
+  }),
+  body: Joi.object().keys({
+    name: Joi.string(),
+    duration: Joi.number(),
+    maxGroupSize: Joi.number(),
+    difficulty: Joi.string().valid('easy', 'medium', 'difficult'),
+    price: Joi.number(),
     priceDiscount: Joi.number(),
     summary: Joi.string(),
     description: Joi.string(),
@@ -24,98 +107,23 @@ const createTour = {
         publicId: Joi.string(),
       })
     ),
-    tours: Joi.object().keys({
-      name: Joi.string(),
-      images: Joi.array().items(Joi.string()),
-      booking: Joi.string(),
-    }),
-    keywords: Joi.array().items(Joi.string()),
-    attraction: Joi.string().custom(objectId),
+    startDates: Joi.array().items(Joi.date().format('DD/MM/YYYY')),
+    secretTour: Joi.boolean().optional(),
     location: Joi.object().keys({
-      type: Joi.string().required(),
+      type: Joi.string(),
       coordinates: Joi.array().items(Joi.number().precision(8)),
       name: Joi.string(),
-      address: Joi.string(),
-    }),
-    openingHours: Joi.string(),
-    highlightSpots: Joi.array().items(
-      Joi.object().keys({
-        type: Joi.string(),
-        coordinates: Joi.array().items(Joi.number()),
-        description: Joi.string(),
-      })
-    ),
-    isAccessibleForFree: Joi.boolean(),
-    publicAccess: Joi.boolean(),
-    slogan: Joi.string(),
-    ratingsAverage: Joi.number(),
-    ratingsQuantity: Joi.number(),
-  }),
-};
-
-const getTours = {
-  query: Joi.object().keys({
-    name: Joi.string(),
-    ratingsAverage: Joi.string(),
-    slug: Joi.string(),
-    attraction: Joi.string(),
-    sortBy: Joi.string(),
-    limit: Joi.number().integer(),
-    page: Joi.number().integer(),
-  }),
-};
-
-const getTour = {
-  params: Joi.object().keys({
-    tourId: Joi.string().custom(objectId),
-    slug: Joi.string(),
-  }),
-};
-
-const updateTour = {
-  params: Joi.object().keys({
-    tourId: Joi.required().custom(objectId),
-  }),
-  body: Joi.object().keys({
-    name: Joi.string(),
-    altName: Joi.string(),
-    mainImage: Joi.object().keys({
-      url: Joi.string(),
-      publicId: Joi.string(),
-    }),
-    images: Joi.array().items(
-      Joi.object().keys({
-        url: Joi.string(),
-        publicId: Joi.string(),
-      })
-    ),
-    summary: Joi.string(),
-    description: Joi.string(),
-    tours: Joi.object().keys({
-      name: Joi.string(),
-      images: Joi.array().items(Joi.string()),
-      booking: Joi.string(),
-    }),
-    keywords: Joi.array().items(Joi.string()),
-    attraction: Joi.string().custom(objectId),
-    location: Joi.object().keys({
       description: Joi.string(),
-      type: Joi.string().required(),
-      coordinates: Joi.array().items(Joi.number().precision(8)),
+      address: Joi.string(),
+      day: Joi.number(),
     }),
-    openingHours: Joi.string(),
-    highlightSpots: Joi.array().items(
-      Joi.object().keys({
-        type: Joi.string(),
-        coordinates: Joi.array().items(Joi.number()),
-        description: Joi.string,
-      })
-    ),
-    isAccessibleForFree: Joi.boolean(),
-    publicAccess: Joi.boolean(),
-    slogan: Joi.string(),
-    ratingsAverage: Joi.number(),
-    ratingsQuantity: Joi.number(),
+    locations: Joi.array().items({
+      description: Joi.string(),
+      coordinates: Joi.array().items(Joi.number()),
+      day: Joi.number(),
+    }),
+    guides: Joi.array().items(Joi.string().custom(objectId)),
+    attraction: Joi.string().custom(objectId),
   }),
 };
 
